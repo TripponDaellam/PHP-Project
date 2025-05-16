eebb2a022d32b813539cd4dfa3e2b094c948f711
Khant Zin Hein <heinkhantzin0@gmail.com>
2025-05-16 14:51:22 +0630

    New

--- Partials/left_nav.php
+++ Partials/left_nav.php
@@ -1,6 +1,6 @@
 <div class="mt-5">
-  <a href="#" class="flex text-orange-600 hover:bg-gray-300 px-4 h-9 justify-left items-center text-sm">Home</a>
-  <a href="#" class="flex text-orange-600 hover:bg-gray-300 px-4 h-9 justify-left items-center text-sm">Questions</a>
+  <a href="index.php" class="flex text-orange-600 hover:bg-gray-300 px-4 h-9 justify-left items-center text-sm">Home</a>
+  <a href="Question.php" class="flex text-orange-600 hover:bg-gray-300 px-4 h-9 justify-left items-center text-sm">Questions</a>
   <a href="#" class="flex text-orange-600 hover:bg-gray-300 px-4 h-9 justify-left items-center text-sm">Tags</a>
   <a href="#" class="flex text-orange-600 hover:bg-gray-300 px-4 h-9 justify-left items-center text-sm">Saved</a>
   <a href="#" class="flex text-orange-600 hover:bg-gray-300 px-4 h-9 justify-left items-center text-sm">Community</a>
--- index.php
+++ index.php
@@ -13,7 +13,7 @@
   <?php include 'Partials/nav.php'; ?>
 
   <!-- Left Sidebar -->
-  <aside class="fixed top-[67px] left-0 h-[calc(100%-5rem)] w-[180px] bg-white z-10 hidden md:block border border-r-gray-400">
+  <aside class="fixed top-20 left-0 h-[calc(100%-5rem)] w-[180px] bg-white z-10 hidden md:block">
     <?php include 'Partials/left_nav.php'; ?>
   </aside>
 
