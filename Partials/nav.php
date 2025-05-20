c38534f40f1b53979db40c81cc0964d16e641368
Khant Zin Hein <heinkhantzin0@gmail.com>
2025-05-20 13:17:07 +0630

    New

--- Partials/nav.php
+++ Partials/nav.php
@@ -43,7 +43,7 @@
 </div>
 
   
-   <a href=""> <div class="hidden md:flex items-center space-x-4 hover:text-orange-500">
+   <a href="../User/profile.php"> <div class="hidden md:flex items-center space-x-4 hover:text-orange-500">
   <?php if (isset($_SESSION['user_id'])): ?>
    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
@@ -98,11 +98,14 @@
 
   <?php if (isset($_SESSION['user_id'])): ?>
     <div class="flex items-center space-x-2 hover:text-orange-500 space-x-2">
+      <a href="../User/profile.php" class="flex items-center space-x-2">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
   </svg>
       <span class="text-gray-700 hover:text-orange-500">Profile</span>
     </div>
+     </a>
   <?php else: ?>
     <a href="User/Login.php" class="block text-orange-600 hover:underline">Login</a>
     <a href="User/SignUp.php" class="block bg-orange-500 text-white px-3 py-1 rounded hover:bg-orange-600 text-center">Sign Up</a>
