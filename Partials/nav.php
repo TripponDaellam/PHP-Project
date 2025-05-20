7a6b344bef3637ee7194c00a76b426b151d48073
Khant Zin Hein <heinkhantzin0@gmail.com>
2025-05-20 14:38:21 +0630

    New

--- Partials/nav.php
+++ Partials/nav.php
@@ -97,13 +97,11 @@
       </a>
 
   <?php if (isset($_SESSION['user_id'])): ?>
-      <a href="../User/profile.php"> 
-    <div class="flex items-center space-x-2 hover:text-orange-500 space-x-2">
+      <a href="../User/profile.php" class="flex items-center text-gray-700 hover:text-orange-500 space-x-2"> 
    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
   </svg>
       <span class="text-gray-700 hover:text-orange-500">Profile</span>
-    </div>
   </a>
   <?php else: ?>
     <a href="User/Login.php" class="block text-orange-600 hover:underline">Login</a>
