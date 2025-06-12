66eb18577ff9a90c067a03c6a14bdaa1f21ce63d
TripponDaellam <myo663957@gmail.com>
2025-06-12 15:55:46 +0630

    reset link email

--- User/ForgotPass.php
+++ User/ForgotPass.php
@@ -35,7 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
       $mail->addAddress($email);
       $mail->isHTML(true);
       $mail->Subject = 'Reset your Method Flow password';
-      // $reset_link = "https://localhost/User/resetPassword.php?token=$token&email=$email";
+      // $reset_link = "localhost/User/resetPassword.php?token=$token&email=$email";
       $reset_link = "https://www.youporn.com/";
       $mail->Body = "
       <html>
